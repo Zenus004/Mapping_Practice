9a7adc4f1d6ff29e5f2a925fbbcea385ce1a4c74
Zenus004 <satyajyoti05@gmail.com>
2024-09-12 17:20:08 +0530

    update

--- src/main/java/com/cls0309/mapping/MappingApplication.java
+++ src/main/java/com/cls0309/mapping/MappingApplication.java
@@ -36,9 +36,9 @@ public class MappingApplication {
         courseRepository.save(course3);
 
         // Create new student
-        Student student1 = new Student();
-        student1.setId(101);
-        student1.setName("Satyajyoti");
+        Student student = new Student();
+        student.setId(101);
+        student.setName("Satyajyoti");
 
         Student student2 = new Student();
         student2.setId(102);
@@ -49,37 +49,27 @@ public class MappingApplication {
         address1.setAdd_id(1);
         address1.setCity("Khordha");
         address1.setState("Odisha");
-        address1.setStudent(student1);
+        address1.setStudent(student);
 
         Address address2 = new Address();
         address2.setAdd_id(2);
         address2.setCity("Jatni");
         address2.setState("Odisha");
-        address2.setStudent(student1);
-
-        Address address3 = new Address();
-        address3.setAdd_id(3);
-        address3.setCity("Butwal");
-        address3.setState("Nepal");
-        address3.setStudent(student2);
+        address2.setStudent(student);
 
         // Adding addresses to lists
         List<Address> addressList1 = new ArrayList<>();
         addressList1.add(address1);
         addressList1.add(address2);
-        student1.setAddressList(addressList1);
+        student.setAddressList(addressList1);
 
-        List<Address> addressList2 = new ArrayList<>();
-        addressList2.add(address2);
-        addressList2.add(address3);
-        student2.setAddressList(addressList2);
 
         // Set courses
         List<Course> courses1 = new ArrayList<>();
         courses1.add(course1);
         courses1.add(course2);
         courses1.add(course3);
-        student1.setCourses(courses1);
+        student.setCourses(courses1);
 
         List<Course> courses2 = new ArrayList<>();
         courses2.add(course1);
@@ -88,7 +78,7 @@ public class MappingApplication {
         student2.setCourses(courses2);
 
         // Save students with addresses and courses
-        studentRepository.save(student1);
+        studentRepository.save(student);
         studentRepository.save(student2);
     }
 }
